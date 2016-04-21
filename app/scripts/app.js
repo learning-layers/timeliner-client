ef3d3d3d6bf2b97b7b3b17d1c004427770405294
Pjotr Savitski <gnum@tlu.ee>
2016-04-21 13:06:52 +0300

    Adding freshly generated Angular appp.

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name timelinerApp
+ * @description
+ * # timelinerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('timelinerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ]);
