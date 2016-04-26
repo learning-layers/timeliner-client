786edc5149f50decb5f1861f4b7eb7955f046471
Pjotr Savitski <gnum@tlu.ee>
2016-04-26 15:53:27 +0300

    Fixes learning-layers/timeliner#2
    The real commit happened before, this one just fixes tests.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
+      'app/scripts/directives/htk.common/module.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
