f09f2a052a4a4290fd1a6b050172f40a73e9b17b
sanderaido <aido@tlu.ee>
2016-04-27 16:08:29 +0300

    Made the registration error more specific based on server response (fixes learning-layers/timeliner#9)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,8 +18,12 @@ angular.module('timelinerApp')
         $scope.success = success;
         $scope.registerEmail = '';
       }, function (error) {
+        if(error.status == 409){
+          $scope.error = 409;
+        } else {
+          $scope.error = error.status;
+        }
         $scope.updating = false;
-        $scope.error = error;
       });
     };
 
