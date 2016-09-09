322cffbb73d243a8ca687832e9d43c9d3a121e1f
Pjotr Savitski <gnum@tlu.ee>
2016-09-09 22:50:17 +0300

    References learning-layers/timeliner#61, added basic icons.
    Only a few cases are handled at the moment.

--- app/scripts/controllers/resources-panel.js
+++ app/scripts/controllers/resources-panel.js
@@ -20,4 +20,29 @@ angular.module('timelinerApp')
     $scope.getResourceOpenIcon = function(resource) {
       return ( resource && resource.file )  ? 'file_download' : 'open_in_new';
     };
+
+    $scope.getIcon = function(resource) {
+      var icon = 'mdi-help';
+
+      if ( resource.url ) {
+        icon = 'mdi-web';
+      } else if ( resource.file ) {
+        switch(resource.file.type) {
+          case 'image/jpeg':
+          case 'image/jpg':
+          case 'image/png':
+          case 'image/gif':
+          case 'image/tiff':
+            icon = 'mdi-file-image';
+            break;
+          case 'application/pdf':
+            icon = 'mdi-file-pdf';
+            break;
+          default:
+            icon = 'mdi-file';
+        }
+      }
+
+      return icon;
+    };
   });
