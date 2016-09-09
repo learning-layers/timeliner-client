e705d49139908d23fd22a5afc2c0e20314ade82d
Pjotr Savitski <gnum@tlu.ee>
2016-09-09 17:19:03 +0300

    References learning-layers/timeliner#62, one more fix.
    Made sure FormData with file upload capabilities is used in both create
    and update cases for reasource.

--- app/scripts/controllers/create-update-resource-dialog.js
+++ app/scripts/controllers/create-update-resource-dialog.js
@@ -9,6 +9,21 @@
  */
 angular.module('timelinerApp')
   .controller('CreateUpdateResourceDialogCtrl', function ($scope, $mdDialog, $log, project, resource, ProjectsService, SystemMessagesService) {
+    function getFormData() {
+      var formData = new FormData();
+      formData.append('title', $scope.model.title);
+      formData.append('description', $scope.model.description);
+
+      if ( $scope.model.url ) {
+        formData.append('url', $scope.model.url);
+      }
+      if ( $scope.model.file ) {
+        formData.append('file', $scope.model.file);
+      }
+
+      return formData;
+    }
+
     $scope.updating = false;
 
     $scope.model = {
@@ -45,10 +60,7 @@ angular.module('timelinerApp')
         ProjectsService.updateResource({
           project: project._id,
           id: $scope.model._id
-        }, {
-          title: $scope.model.title,
-          description: $scope.model.description,
-        }, function(response) {
+        }, getFormData(), function(response) {
           $log.debug('Resource update success', response);
           $scope.updating = false;
           $mdDialog.hide(response.data);
@@ -58,20 +70,9 @@ angular.module('timelinerApp')
           $scope.updating = false;
         });
       } else {
-        var form = new FormData();
-        form.append('title', $scope.model.title);
-        form.append('description', $scope.model.description);
-
-        if ( $scope.model.url ) {
-          form.append('url', $scope.model.url);
-        }
-        if ( $scope.model.file ) {
-          form.append('file', $scope.model.file);
-        }
-
         ProjectsService.createResource({
           project: project._id
-        }, form, function(response) {
+        }, getFormData(), function(response) {
           $log.debug('Resource creation success', response);
           $scope.updating = false;
           $mdDialog.hide(response.data);
