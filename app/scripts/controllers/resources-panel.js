3ed52dfe7ccfe6b4ba82ee759d75666b24c88ec6
Pjotr Savitski <gnum@tlu.ee>
2016-09-13 12:11:09 +0300

    References learning-layers/timeliner#61, extended file type detection.
    This affects displying correct icon based on file type.

--- app/scripts/controllers/resources-panel.js
+++ app/scripts/controllers/resources-panel.js
@@ -28,15 +28,68 @@ angular.module('timelinerApp')
         icon = 'mdi-web';
       } else if ( resource.file ) {
         switch(resource.file.type) {
+          // file-check, file-cloud,
+          // file-export, file-find, file-hidden, file-import, file-lock,
+          // file-multiple, file-outline, file-restore,
+          // file-send, file-tree, file-xml
           case 'image/jpeg':
-          case 'image/jpg':
           case 'image/png':
           case 'image/gif':
           case 'image/tiff':
+          case 'image/bmp':
+          case 'image/x-icon':
+          case 'image/svg+xml':
             icon = 'mdi-file-image';
             break;
           case 'application/pdf':
             icon = 'mdi-file-pdf';
+            break;
+          /*case '':
+            icon = 'mdi-file-chart';
+            break;*/
+          case 'text/csv':
+          case 'text/tab-separated-values':
+            icon = 'mdi-file-delimited';
+            break;
+          case 'application/x-abiword':
+          case 'text/plain':
+            icon = 'mdi-file-document';
+            break;
+          case 'application/vnd.ms-excel':
+          case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            icon = 'mdi-file-excel';
+            break;
+          case 'application/vnd.ms-powerpoint':
+          case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+            icon = 'mdi-file-powerpoint';
+            break;
+          case 'application/msword':
+          case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            icon = 'mdi-file-word';
+            break;
+          case 'application/ogg':
+          case 'audio/mp3':
+          case 'audio/midi':
+          case 'audio/x-m4a':
+          case 'audio/amr':
+          case 'audio/mpeg':
+          case 'audio/wav':
+          case 'audio/x-ms-wma':
+          case 'audio/x-ms-wmv':
+          case 'audio/webm':
+          case 'audio/x-aac':
+          case 'audio/x-aiff':
+          case 'audio/mp4':
+            icon = 'mdi-file-music';
+            break;
+          case 'video/avi':
+          case 'video/mpeg':
+          case 'video/quicktime':
+          case 'video/webm':
+          case 'video/x-msvideo':
+          case 'video/x-flv':
+          case 'video/mp4':
+            icon = 'mdi-file-video';
             break;
           default:
             icon = 'mdi-file';
