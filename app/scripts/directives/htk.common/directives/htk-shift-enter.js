b91eb42041da51bca945d3f01a095d2d96826638
Sander Aido <aido@tlu.ee>
2016-11-07 16:52:48 +0200

    Renamed chat input module

--- app/scripts/directives/htk.common/directives/htk-shift-enter.js
+++ app/scripts/directives/htk.common/directives/htk-shift-enter.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('htk.common')
-  .directive('htkChatInput', function () {
+  .directive('htkShiftEnter', function () {
     return function (scope, element, attrs) {
       element.bind("keydown keypress", function (event) {
         if(event.which === 13 && !event.shiftKey) {
           scope.$apply(function (){
-            scope.$eval(attrs.htkChatInput);
+            scope.$eval(attrs.htkShiftEnter);
           });
 
           event.preventDefault();
